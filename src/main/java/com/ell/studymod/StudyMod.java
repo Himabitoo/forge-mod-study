538f4a207c263754721efb23ae93f65042d996ee
Jun Kojima <luxeno103@gmail.com>
2023-03-26 20:12:03 +0900

    update

---
+++ src/main/java/com/ell/studymod/StudyMod.java
@@ -0,0 +1,35 @@
+package com.ell.studymod;
+
+import com.ell.studymod.item.ModItems;
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(StudyMod.MOD_ID)
+public class StudyMod {
+
+    public static final String MOD_ID = "studymod";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public StudyMod()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
+        MinecraftForge.EVENT_BUS.register(this);
+
+        modEventBus.addListener(this::addCreative);
+    }
+
+    private void addCreative(CreativeModeTabEvent.BuildContents event){
+        if(event.getTab() == CreativeModeTabs.INGREDIENTS){
+            event.accept(ModItems.RED_DIAMOND);
+        }
+    }
+
+}
---
+++ src/main/java/com/ell/studymod/event/PlayerListener.java
@@ -0,0 +1,25 @@
+package com.ell.studymod.event;
+
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.event.level.BlockEvent.BreakEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
+public class PlayerListener{
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    @SubscribeEvent
+    public static void onDestroyBlock(BreakEvent event){
+
+        Player player = event.getPlayer();
+        Block block = event.getState().getBlock();
+        String msg = "You Break " + block.getName();
+        player.sendSystemMessage(Component.nullToEmpty(msg));
+    }
+}
