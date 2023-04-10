c72d4d359203cc80876409b43205a60c93defec8
Jun Kojima <luxeno103@gmail.com>
2023-04-10 19:51:49 +0900

    edit

--- src/main/java/com/ell/studymod/datagen/ModBlockStateProvider.java
+++ src/main/java/com/ell/studymod/datagen/ModBlockStateProvider.java
@@ -1,4 +1,26 @@
 package com.ell.studymod.datagen;
 
-public class ModBlockStateProvider {
+import com.ell.studymod.StudyMod;
+import com.ell.studymod.block.ModBlocks;
+import net.minecraft.data.PackOutput;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModBlockStateProvider extends BlockStateProvider {
+
+    public ModBlockStateProvider(PackOutput output, ExistingFileHelper exFileHelper) {
+        super(output, StudyMod.MOD_ID, exFileHelper);
+    }
+
+    @Override
+    protected void registerStatesAndModels() {
+        blockWithItem(ModBlocks.DEEPSLATE_THUNDER_DIAMOND_ORE);
+    }
+
+    private void blockWithItem(RegistryObject<Block> blockRegistryObject){
+        simpleBlockWithItem(blockRegistryObject.get(),cubeAll(blockRegistryObject.get()));
+    }
+
 }
